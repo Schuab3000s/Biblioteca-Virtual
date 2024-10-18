f6cf10ce087855fd7f74dc34708e7d07b45875f6
Kevin Schuab <kevinschuab@gmail.com>
2024-10-18 20:06:34 -0300

    add o projeto

---
+++ Etapa 5/bibliotecavirtual/bibliotecavirtual/src/main/java/com/senac/bibliotecavirtual/BibliotecavirtualApplication.java
@@ -0,0 +1,13 @@
+package com.senac.bibliotecavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecavirtualApplication.class, args);
+	}
+
+}
---
+++ Etapa 5/bibliotecavirtual/bibliotecavirtual/src/test/java/com/senac/bibliotecavirtual/BibliotecavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.bibliotecavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
